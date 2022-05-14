b08436517f7fcfd6f41e0050d2a40feface2602f
E2077 <88936201+E2077@users.noreply.github.com>
2022-05-14 20:40:19 +0800

    饿了么吃货豆

--- bak/elmyq.js
+++ bak/elmyq.js
@@ -2,7 +2,7 @@
 /*@萝卜
 APP : 饿了么
 饿了么吃货豆，需要点外卖同学的福利
-脚本说明：目前只支持部分任务，500，1000吃货豆换无门槛外卖红包
+脚本说明：目前只支持部分任务，500， 1000吃货豆换无门槛外卖红包
 重写：https://h5.ele.me/svip/task-list url script-request-header https://cdn.jsdelivr.net/gh/LubooC/Script@main/ELM/elmchd.js
 青龙环境变量  export elmck='.............'
 抓包 h5.ele.me 域名下的任何url 请求头中的Cookie
